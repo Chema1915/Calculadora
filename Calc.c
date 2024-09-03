45c3cda730d43c22069aaeef2837029a8922dd28
Chema1915 <79484793+Chema1915@users.noreply.github.com>
2024-09-03 12:53:33 -0600

    Update Calc.c
    Division

--- Calc.c
+++ Calc.c
@@ -27,3 +27,21 @@ do {
     scanf("%f", &num1);
     printf("Ingrese el segundo número: ");
     scanf("%f", &num2);
+
+switch(opcion){
+case 4:
+    if (num2 !=0){
+        resultado = num1/num2;
+        printf("El resultado de %.2f / %.2f = %.2f/n", num1, num2, resultado);}
+    else{
+        printf("Error, no se puede dividir entre 0/n");
+    }
+    break;
+default:
+    printf("Opcion no valida");
+    break;
+}
+}while (opcion !=5){
+return 0;
+}
+}
